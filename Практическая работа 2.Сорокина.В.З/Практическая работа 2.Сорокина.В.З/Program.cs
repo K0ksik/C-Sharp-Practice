fbf5391cc6d7c0c633bc65a6828179c36bd2d764
Veronika <vodocka2280@gmail.com>
2025-02-13 11:04:20 +0300

    Практическая работа 2

---
+++ Практическая работа 2.Сорокина.В.З/Практическая работа 2.Сорокина.В.З/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+class Rectangle
+{
+    public int Width { get; set; }
+    public int Height { get; set; }
+    public static string Text;
+
+    public Rectangle(int width, int height)
+    {
+        Width = width;
+        Height = height;
+    }
+    
+    public Rectangle(Rectangle rectangle)
+    {
+        Width = rectangle.Width;
+        Height = rectangle.Height;
+    }
+    public Rectangle()
+    {
+        Width = 10;
+        Height = 10;
+    }
+    static Rectangle()
+    {
+        Text = "Прямоугольник";
+        Console.WriteLine(Text);
+    }
+
+    ~Rectangle()
+    {
+        Console.WriteLine($"Деструктор.");
+    }
+
+    public int Area()
+    {
+        return Width * Height;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle rectangle = new Rectangle(5,10);
+        Console.WriteLine(Rectangle.Text);
+        Console.WriteLine($"Площадь: {rectangle.Area()}");
+
+    }
+}
